e1fcdb041a931bb25e2459e741188265ab2f505d
Alejandro <aleva2002@gmail.com>
2023-01-23 19:24:59 +0100

    [NEW] Project created

---
+++ app/src/main/java/dam/alejandro/a41_interfaces/MainActivity.java
@@ -0,0 +1,14 @@
+package dam.alejandro.a41_interfaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
