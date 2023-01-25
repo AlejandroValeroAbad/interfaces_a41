3cc34ebceb70c1ce1b81ed745e1c26919bfcf80e
Alejandro <aleva2002@gmail.com>
2023-01-25 13:36:19 +0100

    [NEW] Tasca 5

--- app/src/main/java/dam/alejandro/a41_interfaces/MainActivity.java
+++ app/src/main/java/dam/alejandro/a41_interfaces/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tasca4);
+        setContentView(R.layout.tasca5);
     }
 }
